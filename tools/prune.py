8da189cab7f98f646eef41ab21f7712b8d82e117
shuang1330 <shuangli1330@gmail.com>
2017-06-27 13:32:21 +0200

    added option for classification-based pruning

--- tools/prune.py
+++ tools/prune.py
@@ -11,7 +11,7 @@ from model.test import test_net
 
 import os
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]="1"
+os.environ["CUDA_VISIBLE_DEVICES"]="2"
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
 import tensorflow as tf
@@ -23,47 +23,21 @@ from nets.vgg16 import vgg16
 from tensorflow.python import pywrap_tensorflow
 from prune_with_classification_guidance import detect_diff_all
 
-# Below are variables that needs checking every time
-num_classes = 21
-old_filter_num = (64,64,128,128,256,256,256,512,512,512,512,512,512,512)
-new_filter_num = (64,64,128,128,256,256,256,512,512,512,128,512,512,512)
-# name_scopes = ('vgg_16/conv1/conv1_1','vgg_16/conv1/conv1_2',
-#                 'vgg_16/conv2/conv2_1','vgg_16/conv2/conv2_2',
-#                 'vgg_16/conv3/conv3_1','vgg_16/conv3/conv3_2',
-#                 'vgg_16/conv3/conv3_3','vgg_16/conv4/conv4_1',
-#                 'vgg_16/conv4/conv4_2','vgg_16/conv4/conv4_3',
-#                 'vgg_16/conv5/conv5_1','vgg_16/conv5/conv5_2',
-#                 'vgg_16/conv5/conv5_3','vgg_16/rpn_conv/3x3')
-names = ('weights','biases')
-# Defined path for loading original weights from ckpy files
-demonet = 'vgg16_faster_rcnn_iter_70000.ckpt'
-dataset = 'voc_2007_trainval'
-tfmodel = os.path.join('../output','vgg16',dataset, 'default', demonet)
-if not os.path.isfile(tfmodel + '.meta'):
-    raise IOError('{:s} not found'.format(tfmodel + '.meta'))
-# Defined path for saving pruned weights to npy files
-weights_name = 'random_pruned_conv11_momentum.npy'
-folder_path = '../output/pruning/'
-if not os.path.exists(folder_path):
-    os.makedirs(folder_path)
-weights_path = os.path.join(folder_path,weights_name)
-
-heatmap_path = './activations_res/res.npy'
-heatmap_all_ind = {}
-
-def choose_filters(current_sum,layer_ind,heatmap_all_ind,
-                    MAGNITUTE,RANDOM,CLASSIFICATION_BASED):
+def choose_filters(current_sum,layer_ind,heatmap_all_ind=None,
+                    MAGNITUTE=False,RANDOM=False,CLASSIFICATION_BASED=False):
     if CLASSIFICATION_BASED:
-        return heatmap_all_ind['%dth_acts'%layer_ind]
+        print 'choose pruning guided by classfiication mode'
+        return heatmap_all_ind['%dth_acts'%(layer_ind+1)]
     if RANDOM:
-        return shuffle(range(len(current_sum)))
+        print 'choose random pruning mode'
+        shuffled_list = range(len(current_sum))
+        shuffle(shuffled_list)
+        return shuffled_list
     if MAGNITUTE:
-        print 'choosing this mode'
-        print np.argsort(current_sum)
+        print 'choosing magnitute mode'
         return np.argsort(current_sum)
 
-
-def filter(dic,RANDOM,MGNITUTE=False,
+def filter(dic,old_filter_num,new_filter_num,heatmap_path=None,RANDOM=False,MAGNITUTE=False,
             CLASSIFICATION_BASED=False):
     '''
     modify the weights_dic according the pruning rule(new_filter_num)
@@ -86,51 +60,53 @@ def filter(dic,RANDOM,MGNITUTE=False,
                 biases.append(dic[name_scope][name]['value'])
                 b_momentum.append(dic[name_scope][name]['Momentum'])
 
-    diff = [(old_filter_num[ind] - new_filter_num[ind]) \
-            for ind in range(len(old_filter_num))]
-    CLASSIFICATION_BASED=True
+    diff = [(old_filter_num[ind] - new_filter_num[ind])
+            for ind in range(len(old_filter_num))] #
+    heatmap_all_ind={}
     if CLASSIFICATION_BASED:
         heatmap_all_ind = detect_diff_all(heatmap_path)
     current_ind = 0
     pre_ind = 0
     if diff[0] != 0: # for the oth layer
         current_sum = np.sum(weights[0], axis = (0,1,2))
-        current_ind = choose_filters(current_sum,0,
-                                    heatmap_all_ind,
-                                    RANDOM,MGNITUTE,
-                                    CLASSIFICATION_BASED=True)
+        current_ind = choose_filters(current_sum,0,heatmap_all_ind,
+                                    RANDOM=RANDOM,MAGNITUTE=MAGNITUTE,
+                                    CLASSIFICATION_BASED=CLASSIFICATION_BASED)
         weights[0] = np.delete(weights[0], current_ind[:diff[0]], axis = 3)
         biases[0] = np.delete(biases[0],current_ind[:diff[0]], axis = 0)
         if w_momentum[0] is not None:
-            w_momentum[0] = np.delete(w_momentum[0], \
-            current_ind[:diff[0]], axis = 3)
-            b_momentum[ind] = np.delete(b_momentum[ind], \
-            current_ind[:diff[0]], axis = 0)
+            w_momentum[0] = np.delete(w_momentum[0],
+                                        current_ind[:diff[0]],
+                                        axis = 3)
+            b_momentum[ind] = np.delete(b_momentum[ind],
+                                        current_ind[:diff[0]],
+                                        axis = 0)
 
     pre_ind = current_ind
     current_ind = None
     for ind in range(1,len(old_filter_num)): # for every layer
         if diff[ind-1] != 0:
-            weights[ind] = np.delete(weights[ind], pre_ind[:diff[ind-1]], \
-            axis = 2)
+            weights[ind] = np.delete(weights[ind], pre_ind[:diff[ind-1]],
+                                    axis = 2)
             if diff[ind] == 0:
                 pre_ind = None
         if diff[ind] != 0:
             current_sum = np.sum(weights[ind],axis = (0,1,2))
-            current_ind = choose_filters(current_sum,ind,
-                                        heatmap_all_ind,
-                                        RANDOM,MGNITUTE,
-                                        CLASSIFICATION_BASED=True)
+            current_ind = choose_filters(current_sum,ind,heatmap_all_ind,
+                                        RANDOM=RANDOM,MAGNITUTE=MAGNITUTE,
+                                        CLASSIFICATION_BASED=CLASSIFICATION_BASED)
             # print current_ind
-            weights[ind] = np.delete(weights[ind], current_ind[:diff[ind]], \
-            axis = 3)
-            biases[ind] = np.delete(biases[ind],current_ind[:diff[ind]], \
-            axis = 0 )
+            weights[ind] = np.delete(weights[ind], current_ind[:diff[ind]],
+                                    axis = 3)
+            biases[ind] = np.delete(biases[ind],current_ind[:diff[ind]],
+                                    axis = 0 )
             if w_momentum[ind] is not None:
-                w_momentum[ind] = np.delete(w_momentum[ind], \
-                current_ind[:diff[ind]], axis = 3)
-                b_momentum[ind] = np.delete(b_momentum[ind], \
-                current_ind[:diff[ind]], axis = 0)
+                w_momentum[ind] = np.delete(w_momentum[ind],
+                                            current_ind[:diff[ind]],
+                                            axis = 3)
+                b_momentum[ind] = np.delete(b_momentum[ind],
+                                            current_ind[:diff[ind]],
+                                            axis = 0)
             pre_ind = current_ind
             current_ind = None
 
@@ -148,8 +124,9 @@ def filter(dic,RANDOM,MGNITUTE=False,
         ind += 1
     return dic
 
-def prune_net_for_training(tfmodel,weights_path,SAVE=False,
-                            RANDOM=False,CLASSIFICATION_BASED=False):
+def prune_net_for_training(tfmodel,weights_path,old_filter_num,new_filter_num,heatmap_path=None,
+                            SAVE=False,RANDOM=False,MAGNITUTE=False,
+                            CLASSIFICATION_BASED=False):
     '''
     prune weights in ckpt files
     save results in npy files
@@ -165,11 +142,11 @@ def prune_net_for_training(tfmodel,weights_path,SAVE=False,
         with tf.Session(config=tfconfig, graph=g1).as_default() as sess:
             #load network
             net = vgg16(batch_size=1)
-            net.create_architecture(sess,'TRAIN',num_classes,tag='default',
+            net.create_architecture(sess,'TRAIN',21,tag='default',
                                     anchor_scales=[8,16,32],
-                                    filter_num=old_filter_num)
-            for var in tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES):
-                print(var.name,var.get_shape())
+                                    filter_num=[64,64,128,128,256,256,256,512,512,512,512,512,512,512])#
+            # for var in tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES):
+            #     print(var.name,var.get_shape())
             saver = tf.train.Saver()
             saver.restore(sess,tfmodel)
             print 'Loaded network {:s}'.format(tfmodel)
@@ -181,21 +158,21 @@ def prune_net_for_training(tfmodel,weights_path,SAVE=False,
                 name_scopes.append('/'.join(name_list[:-1]))
 
             for name_scope in name_scopes:
-                for name in names:
+                for name in ('weights','biases'):#
                     tensor_name = '{}/{}'.format(name_scope,name)
                     momentum_name = '{}/Momentum'.format(tensor_name)
                     if name_scope not in dic:
                         try:
                             dic[name_scope] = \
-                            {name:{'value':reader.get_tensor(tensor_name),\
+                            {name:{'value':reader.get_tensor(tensor_name),
                             'Momentum':reader.get_tensor(momentum_name)}}
                         except:
                             dic[name_scope] = \
-                            {name:{'value':reader.get_tensor(tensor_name),\
+                            {name:{'value':reader.get_tensor(tensor_name),
                             'Momentum':None}}
                     else:
                         dic[name_scope][name] = \
-                        {'value':reader.get_tensor(tensor_name),\
+                        {'value':reader.get_tensor(tensor_name),
                         'Momentum':None}
                         try:
                             dic[name_scope][name]['Momentum'] = \
@@ -204,24 +181,56 @@ def prune_net_for_training(tfmodel,weights_path,SAVE=False,
                             continue
 
     # filter the weights
-    dic = filter(dic,RANDOM,)
-    for name_scope in dic:
-        for name in dic[name_scope]:
-            try:
-                print 'After filtering, the variable {}/{}/Momentum has shape {}'.format(
-                name_scope, name, dic[name_scope][name]['Momentum'].shape)
-            except:
-                print 'After filtering, the variable {}/{}/Momentum is: '.format(
-                name_scope, name, dic[name_scope][name]['Momentum'])
+    dic = filter(dic,old_filter_num,new_filter_num,heatmap_path,RANDOM,MAGNITUTE,CLASSIFICATION_BASED)
+
+    # display the filtered weights' shape
+    # for name_scope in dic:
+    #     for name in dic[name_scope]:
+    #         try:
+    #             print 'After filtering, the variable {}/{} has shape {}'.format(
+    #             name_scope, name, dic[name_scope][name]['Momentum'].shape)
+    #         except:
+    #             print 'After filtering, the variable {}/{}/Momentum is: '.format(
+    #             name_scope, name, dic[name_scope][name]['Momentum'])
 
     # save the pruned weights to npy file
     if SAVE:
         np.save(weights_path, dic)
         print 'The weights are saved in {}'.format(weights_path)
-    return True
+    return dic
 
 if __name__ == '__main__':
-    prune_net_for_training(tfmodel,weights_path,RANDOM=True)
+    # Below are variables that needs checking every time
+    num_classes = 21
+    old_filter_num = (64,64,128,128,256,256,256,512,512,512,512,512,512,512)
+    new_filter_num = (64,64,128,128,256,256,256,512,512,512,128,512,512,512)
+    # name_scopes = ('vgg_16/conv1/conv1_1','vgg_16/conv1/conv1_2',
+    #                 'vgg_16/conv2/conv2_1','vgg_16/conv2/conv2_2',
+    #                 'vgg_16/conv3/conv3_1','vgg_16/conv3/conv3_2',
+    #                 'vgg_16/conv3/conv3_3','vgg_16/conv4/conv4_1',
+    #                 'vgg_16/conv4/conv4_2','vgg_16/conv4/conv4_3',
+    #                 'vgg_16/conv5/conv5_1','vgg_16/conv5/conv5_2',
+    #                 'vgg_16/conv5/conv5_3','vgg_16/rpn_conv/3x3')
+    names = ('weights','biases')
+    # Defined path for loading original weights from ckpy files
+    demonet = 'vgg16_faster_rcnn_iter_70000.ckpt'
+    dataset = 'voc_2007_trainval'
+    tfmodel = os.path.join('../output','vgg16',dataset, 'default', demonet)
+    if not os.path.isfile(tfmodel + '.meta'):
+        raise IOError('{:s} not found'.format(tfmodel + '.meta'))
+    # Defined path for saving pruned weights to npy files
+    weights_name = 'random_prune_conv11_to128_with_momentum.npy'
+    folder_path = '../output/pruning/'
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    weights_path = os.path.join(folder_path,weights_name)
+    # Only useful when classification-based method is chosen
+    heatmap_path = './activations_res/res.npy'
+    heatmap_all_ind = {}
+
+    prune_net_for_training(tfmodel,weights_path,heatmap_path=heatmap_path,
+                            SAVE=True,RANDOM=True,MAGNITUTE=False,
+                            CLASSIFICATION_BASED=False)
     '''
     load the new weights to a new graph,
     test the pruned network
@@ -245,9 +254,12 @@ if __name__ == '__main__':
                     for name in weights_dic[name_scope]:
                         var = tf.get_variable(name)
                         sess.run(var.assign(weights_dic[name_scope][name]['value']))
-                        print 'assign pretrain model to {}/{}'.format(name_scope,name)
+            print 'assigned pruned weights to the pruned model'
 
             # test the new model
             imdb = get_imdb('voc_2007_test')
-            filename = 'demo_pruning'
-            test_net(sess, net, imdb, filename, max_per_image=100)
+            filename = 'demo_pruning/experiments/random'
+            experiment_setup = 'prune_layer11_to128'
+            test_net(sess, net, imdb, filename,
+                    experiment_setup=experiment_setup,
+                    max_per_image=100)
--- tools/prune_with_classification_guidance.py
+++ tools/prune_with_classification_guidance.py
@@ -11,9 +11,6 @@ CLASSES = ('__background__',
 class_to_ind = dict(list(zip(CLASSES, list(range(len(CLASSES))))))
 SUB_CLAS = ('bicycle', 'bus', 'car','motorbike', 'person', 'train')
 
-# define path for loading the activations_versus_classes array
-hm_path = './activations_res/res.npy'
-
 def rankmin(x):
     u, inv, counts = np.unique(x, return_inverse=True, return_counts=True)
     csum = np.zeros_like(counts)
@@ -45,7 +42,7 @@ def detect_diff_one_layer(norm_hm_one_layer):
             temp[temp>0.5] = 1
             # diff_ind[clas] = np.argsort(temp)
             diff_ind += temp
-    # diff_ind = np.argsort(diff_ind)
+    diff_ind = np.argsort(diff_ind)[::-1]
     return diff_ind
 
 def detect_diff_all(hm_path):
@@ -61,6 +58,9 @@ def detect_diff_all(hm_path):
     return hm_ind
 
 if __name__=='__main__':
+    # define path for loading the activations_versus_classes array
+    hm_path = './activations_res/res.npy'
     hm_sorted = detect_diff_all(hm_path)
     for key in hm_sorted:
-        print key, np.count_nonzero(hm_sorted[key])
+        print key,hm_sorted[key].shape
+        # print key, np.count_nonzero(hm_sorted[key])
